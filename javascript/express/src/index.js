9b444b2c04d53aff19ac06f3f990ad5deae5f80d
linden5 <827935541@qq.com>
2017-09-03 22:29:35 +0800

    learn to use pug

---
+++ javascript/express/src/index.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+
+app.use(express.static(path.join(__dirname + '/public')));
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname + '/views'));
+
+app.get('/', function(req, res) {
+    res.render('index');
+});
+
+app.get('/blog', function(req, res) {
+    res.render('blog');
+});
+
+app.get('/editor', function(req, res) {
+    res.render('editor');
+});
+
+app.listen(3000, function() {
+    console.log('Example app listening on port 3000!');
+});
