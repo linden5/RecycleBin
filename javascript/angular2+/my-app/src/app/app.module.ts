842701fcaa9554af284f27a8f941ff4bfa943cea
linden5 <827935541@qq.com>
2018-04-13 18:27:01 +0800

    完成angular教程,tour of heros

--- javascript/angular2+/my-app/src/app/app.module.ts
+++ javascript/angular2+/my-app/src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
@@ -11,7 +12,8 @@ import { MessagesComponent } from './messages/messages.component';
 import { MessageService } from './message.service';
 import { AppRoutingModule } from './/app-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component'
-
+import { InMemoryDataService } from './in-memory-data.service';
+import { HeroSearchComponent } from './hero-search/hero-search.component';
 
 @NgModule({
   declarations: [
@@ -19,12 +21,17 @@ import { DashboardComponent } from './dashboard/dashboard.component'
     HeroesComponent,
     HeroDetailComponent,
     MessagesComponent,
-    DashboardComponent
+    DashboardComponent,
+    HeroSearchComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false }
+    )
   ],
   providers: [
     HeroService,
--- javascript/angular2+/my-app/src/app/heroes/heroes.component.ts
+++ javascript/angular2+/my-app/src/app/heroes/heroes.component.ts
@@ -21,4 +21,18 @@ export class HeroesComponent implements OnInit {
     this.heroService.getHeroes()
       .subscribe(heros => this.heros = heros);
   }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) {return;}
+    this.heroService.addHero({name} as Hero)
+      .subscribe(hero => {
+        this.heros.push(hero);
+      })
+  }
+
+  delete(hero: Hero): void {
+    this.heros = this.heros.filter(h => h !== hero);
+    this.heroService.deleteHero(hero).subscribe();
+  }
 }
